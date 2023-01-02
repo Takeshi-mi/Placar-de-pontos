df450d286ef268850416bc8be2a8e8615eca1608
Takeshi Miura <101356765+Takeshi-mi@users.noreply.github.com>
2023-01-02 16:07:41 -0300

    first commit

---
+++ src/placarpontuacao/PlacarPontuacao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package placarpontuacao;
+
+import view.telaInicial;
+
+/**
+ *
+ * @author Takeshi
+ */
+public class PlacarPontuacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        telaInicial inicial = new telaInicial();
+        inicial.setVisible(true);
+    }
+    
+}
